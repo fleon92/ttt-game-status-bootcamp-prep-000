cf5ce042d1812c37aad8b00161c82a1ed4737e34
Felipe Leon <felipeleon92@gmail.com>
2018-01-10 00:08:04 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,12 +15,12 @@ WIN_COMBINATIONS = [
   [2,5,8]
   ]
   
-    def won?
-    WIN_COMBINATIONS.find do |win_combo|
-      @board[win_combo[0]] == @board[win_combo[1]] && @board[win_combo[0]] == @board[win_combo[2]] && position_taken?(win_combo[1])
-    end
+def won?(board)
+  WIN_COMBINATIONS.find do |win_combo|
+    board[win_combo[0]] == board[win_combo[1]] && board[win_combo[0]] == board[win_combo[2]] && position_taken?(board, win_combo[1])
   end
-  
+end
+
   def full?(board)
    board.none?{|i| i == "" || i == " " || i == nil}
   end
